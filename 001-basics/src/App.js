b75970cce4df64fbfdd8625316d020b98402f307
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-14 17:30:51 +0530

    add basics folder

---
+++ 001-basics/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello JS</h1>
+    <nav className="navBar">
+      <li>Home</li>
+      <li>contact</li>
+      <li>about</li>
+    </nav>
+    <p>lorem ipsum
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt odio delectus ad hic illum minima eaque vero consectetur autem, eligendi, natus nihil asperiores earum, quidem atque. Dolorem excepturi porro molestias.
+      Numquam perspiciatis, optio facere voluptates ipsum in culpa, aperiam, eligendi inventore illum rem cumque. Officia quidem ullam repellendus dolores iusto, excepturi temporibus laudantium quo incidunt quibusdam ab possimus ducimus laboriosam!
+      Blanditiis nam est exercitationem cupiditate tempora soluta, maxime corrupti velit voluptate labore, enim non eligendi, corporis laudantium consequuntur vitae. Possimus ea ipsa, amet labore sint provident dicta neque repellendus suscipit.
+    </p>
+    </>
+  );
+}
+
+export default App;

c623d04e4a8cb5fd30bec02da5d5d7733bf34e98
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-15 10:26:30 +0530

    add About section

--- 002-textutills/src/App.js
+++ 002-textutills/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
     <Navbar navTitle="TitleUtills" homeTilte="Home" aboutTitle="About" searchTitle="Search"/>
-    <TextForm heading="Enter your text to analysis below :"/>
+    {/* <TextForm heading="Enter your text to analysis below :"/> */}
+    <About aboutTitle="About Us"/>
     </>
   );
 }

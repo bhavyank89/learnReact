e62d9dfdac7d8089fdf1ea795b7858367000bd39
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-23 19:22:18 +0530

    add catogaries with react-router-dom

--- 003-newsapp/src/App.js
+++ 003-newsapp/src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <News />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<News country="in" catogary="general" pageSize={3} />}></Route>
+            <Route path='/about' element={<News country="in" catogary="general" pageSize={3} />}></Route>
+            <Route path='/business' element={<News country="in" catogary="business" pageSize={3} />}></Route>
+            <Route path='/entertainment' element={<News country="in" catogary="entertainment" pageSize={3} />}></Route>
+            <Route path='/sports' element={<News country="in" catogary="sports" pageSize={3} />}></Route>
+            <Route path='/technology' element={<News country="in" catogary="technology" pageSize={3} />}></Route>
+            <Route path='/science' element={<News country="in" catogary="science" pageSize={3} />}></Route>
+            <Route path='/health' element={<News country="in" catogary="health" pageSize={3} />}></Route>
+            <Route path='/general' element={<News country="in" catogary="general" pageSize={3} />}></Route>
+          </Routes>
+        </Router>
       </div>
     )
   }

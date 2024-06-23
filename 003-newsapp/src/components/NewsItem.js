9be0b97e9776537727e6b952bc5b34bb62465af2
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-23 11:11:41 +0530

    removed width from newsItem.js

--- 003-newsapp/src/components/NewsItem.js
+++ 003-newsapp/src/components/NewsItem.js
@@ -5,7 +5,7 @@ export default class NewsItem extends Component {
         let { title, description, imgSrc, newsUrl } = this.props;
         return (
             <div>
-                <div className="card my-3" style={{ width: "18rem" }}>
+                <div className="card my-3">
                     <img src={imgSrc ? imgSrc : "https://www.hindustantimes.com/ht-img/img/2024/06/16/1600x900/ANI-20240609316-0_1718533239088_1718533307177.jpg"} style={{ width: "100%", height: "200px" }} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title} ...<span style={{ fontSize: "15px", fontWeight: "500" }}>read More</span></h5>

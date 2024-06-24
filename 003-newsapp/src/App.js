3b7e4b34361a2616de43efcbb1c2a061efb76d55
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-24 18:55:12 +0530

    add top loading bar

--- 003-newsapp/src/App.js
+++ 003-newsapp/src/App.js
@@ -3,25 +3,42 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+import { useState } from 'react';
 
 export default class App extends Component {
   pageSize = 3;
   country = "in";
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({
+      progress: progress
+    })
+  }
+
   render() {
     return (
       <div>
         <Router>
+          <LoadingBar
+            color='#f11946'
+            progress={this.state.progress}
+          />
           <Navbar />
           <Routes>
-            <Route path='/' element={<News country={this.country} category="general" pageSize={this.pageSize} />}></Route>
-            <Route path='/about' element={<News country={this.country} category="general" pageSize={this.pageSize} />}></Route>
-            <Route path='/business' element={<News country={this.country} category="business" pageSize={this.pageSize} />}></Route>
-            <Route path='/entertainment' element={<News country={this.country} category="entertainment" pageSize={this.pageSize} />}></Route>
-            <Route path='/sports' element={<News country={this.country} category="sports" pageSize={this.pageSize} />}></Route>
-            <Route path='/technology' element={<News country={this.country} category="technology" pageSize={this.pageSize} />}></Route>
-            <Route path='/science' element={<News country={this.country} category="science" pageSize={this.pageSize} />}></Route>
-            <Route path='/health' element={<News country={this.country} category="health" pageSize={this.pageSize} />}></Route>
-            <Route path='/general' element={<News country={this.country} category="general" pageSize={this.pageSize} />}></Route>
+            <Route path='/' element={<News setProgress={this.setProgress} country={this.country} category="general" pageSize={this.pageSize} />}></Route>
+            <Route path='/about' element={<News setProgress={this.setProgress} country={this.country} category="general" pageSize={this.pageSize} />}></Route>
+            <Route path='/business' element={<News setProgress={this.setProgress} country={this.country} category="business" pageSize={this.pageSize} />}></Route>
+            <Route path='/entertainment' element={<News setProgress={this.setProgress} country={this.country} category="entertainment" pageSize={this.pageSize} />}></Route>
+            <Route path='/sports' element={<News setProgress={this.setProgress} country={this.country} category="sports" pageSize={this.pageSize} />}></Route>
+            <Route path='/technology' element={<News setProgress={this.setProgress} country={this.country} category="technology" pageSize={this.pageSize} />}></Route>
+            <Route path='/science' element={<News setProgress={this.setProgress} country={this.country} category="science" pageSize={this.pageSize} />}></Route>
+            <Route path='/health' element={<News setProgress={this.setProgress} country={this.country} category="health" pageSize={this.pageSize} />}></Route>
+            <Route path='/general' element={<News setProgress={this.setProgress} country={this.country} category="general" pageSize={this.pageSize} />}></Route>
           </Routes>
         </Router>
       </div>
--- 003-newsapp/src/components/News.js
+++ 003-newsapp/src/components/News.js
@@ -29,16 +29,20 @@ export default class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(10);
         const { currentPage } = this.state;
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=13ea8cd3e420423b9bad251fbca94214&page=${currentPage}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json();
+        this.props.setProgress(70);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
         });
+        this.props.setProgress(100);
     }
 
     async componentDidMount() {

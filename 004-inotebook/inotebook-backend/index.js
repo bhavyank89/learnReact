02616ed7f03e829cf1424c605bdb6b0dc84dde25
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-27 17:43:19 +0530

    add routes

--- 004-inotebook/inotebook-backend/index.js
+++ 004-inotebook/inotebook-backend/index.js
@@ -6,9 +6,12 @@ connectToMongo();
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+//Insteed of listing every routes in our index.js we use app.use
+// app.get('/', (req, res) => { res.send('Hello World!') })
+
+//Available routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)

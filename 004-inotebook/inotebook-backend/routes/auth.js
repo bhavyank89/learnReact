f574cb4abfd30ac07201722a9546226542063d3f
Bhavyank Sarolia <137688063+bhavyank89@users.noreply.github.com>
2024-07-23 10:28:07 +0530

    Delete 004-inotebook directory

--- 004-inotebook/inotebook-backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    obj = { a: 'thios', b: 34 }
-    res.json(obj)
-})
-
-module.exports = router

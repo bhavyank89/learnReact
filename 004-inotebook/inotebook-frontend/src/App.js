fb3edb34658cf6c91a8786e456a299fb74f3d342
bhavyank89 <bhavyanksarolia@gmail.com>
2024-08-07 17:54:43 +0530

    add react-router-dom in App.js

--- 004-inotebook/inotebook-frontend/src/App.js
+++ 004-inotebook/inotebook-frontend/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is inotebook</h1>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/about' element={<About />}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;

5dbb2c0773b948e9d703b80d9d96be92591c374c
bhavyank89 <bhavyanksarolia@gmail.com>
2024-08-13 11:47:38 +0530

    learn useMemo hook

--- 006-hooks/src/App.js
+++ 006-hooks/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import { useState, useEffect, useRef, useContext, useReducer, useCallback, useMemo, useLayoutEffect, useDebugValue } from 'react';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 
 /*
 - Hooks are functions that let you “hook into” React state and lifecycle features from function components. 
@@ -16,19 +16,17 @@ function App() {
   // Creating State
   // useState hook - To manage states. Returns a stateful value and an updater function to update it.
   const [count, setCount] = useState(0);
-  const [name, setName] = useState("John");
-  const [myNum, setMyNum] = useState(0);
   const handleCount = () => {
     // Handling State
     setCount(count + 1);
-    console.log("Count : ", count);
-  }
-  const handleName = () => {
-    (name === "John") ? setName("Ram") : setName("John");
   }
 
   // UseEffect hook - To manage side-effects like API calls, subscriptions, timers, mutations, and more.
   // Run whenever re-rendering occurs
+  const [name, setName] = useState("John");
+  const handleName = () => {
+    (name === "John") ? setName("Ram") : setName("John");
+  }
   useEffect(() => {
     console.log("component mounted...");
     // Runs at first mounting phase
@@ -36,6 +34,7 @@ function App() {
   }, [name]);
 
   // useRef hook - To manipulate DOM without re-rendering
+  const [myNum, setMyNum] = useState(0);
   const inputOne = useRef();
   const inputTwo = useRef();
 
@@ -48,17 +47,57 @@ function App() {
     console.log("Inside handleInputTwo...");
     console.log(inputTwo.current);
   }
+
+  // Use Memo hook - used to memoize expensive operation and prevent re-rendering the expensive operation unnecessary
+  // Returns the memoize value of the operation
+  const [checkNumber, setCheckNumber] = useState(29_00_000);
+  const [numsSize, setnumSize] = useState(30_00_000);
+  const nums = new Array(numsSize).fill(0).map((_, i) => {
+    return {
+      index: i,
+      isMagical: i === checkNumber
+    };
+  });
+  const [number, setNumber] = useState(nums);
+
+  const handleNumber = () => {
+    const newNumsSize = numsSize === 30_00_000 ? 10_00_000 : 30_00_000;
+    const newCheckNumber = checkNumber === 29_00_000 ? 9_00_000 : 29_00_000;
+
+    setnumSize(newNumsSize);
+    setCheckNumber(newCheckNumber);
+    setNumber(new Array(newNumsSize).fill(0).map((_, i) => {
+      return {
+        index: i,
+        isMagical: i === newCheckNumber
+      };
+    }));
+  };
+
+  // const magical = number.find(item=>item.isMagical===true);// Expensive computation
+  const magical = useMemo(() => {
+    return number.find(num => num.isMagical === true);
+  }, [number]);
+
   return (
     <div className="App">
+      {/* useState and useEffect */}
       <h1>Learning useState and useEffect</h1>
       <p>Count : {count}, Name : {name}</p>
-      <button onClick={handleCount}>Increment</button>
-      <button onClick={handleName}>updateName</button>
+      <button onClick={handleCount}>Count : {count}</button>{" "}
+      <button onClick={handleName}>updateName</button><br />
+
+      {/* Learning useMemo */}
+      <h1>Learning useMemo</h1>
+      <p>magical : {magical ? magical.index : "None"}</p>
+      <button onClick={handleNumber}>numsSize : {numsSize}</button>
+
+      {/* Learning useRef */}
       <h1>Learning useRef</h1>
-      <input ref={inputOne} value={myNum} type="number" onChange={(e) => setMyNum(e.target.value)} />
-      <input ref={inputTwo} value={myNum} type="text" onChange={e => { }} />
-      <button onClick={handleInputOne}>inputOne</button>
-      <button onClick={handleInputTwo}>inputTwo</button>
+      <input ref={inputOne} value={myNum} type="number" onChange={(e) => setMyNum(e.target.value)} /><br />
+      <input ref={inputTwo} value={myNum} type="text" onChange={e => { }} /><br />
+      <button onClick={handleInputOne}>inputOne</button>{" "}
+      <button onClick={handleInputTwo}>inputTwo</button><br />
     </div>
   );
 }

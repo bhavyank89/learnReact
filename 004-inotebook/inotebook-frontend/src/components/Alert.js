ce666a78de606d569a7756d6ae173dde5f573d61
bhavyank89 <bhavyanksarolia@gmail.com>
2024-10-02 17:35:19 +0530

    primary alert button

--- 004-inotebook/inotebook-frontend/src/components/Alert.js
+++ 004-inotebook/inotebook-frontend/src/components/Alert.js
@@ -4,7 +4,7 @@ function Alert(props) {
     return (
         <>
             <div className="container flex-row-reverse d-flex">
-                <div className="alert alert-warning text-dark" role="alert">
+                <div className="alert alert-primary text-dark" role="alert">
                     {props.alertMessage}
                 </div>
             </div>
